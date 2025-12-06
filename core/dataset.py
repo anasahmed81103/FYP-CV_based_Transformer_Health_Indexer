61e583e960e790728e488502e5f2b80484dc24a4
Ibrahim6712 <ibrahimjunaid12@gmail.com>
2025-12-06 05:03:41 +0500

    added pmt classifier model in existing model pipeline on backend only

--- core/dataset.py
+++ core/dataset.py
@@ -8,6 +8,12 @@ from PIL import Image
 import torch
 from torch.utils.data import Dataset
 
+
+
+# =======================
+#  Regression Dataset
+# =======================
+
 # List of the 13 parameter column names
 PARAM_COLUMNS = [
     "Oil_leakage_score",
@@ -26,6 +32,15 @@ PARAM_COLUMNS = [
 ]
 
 class TransformerHealthDataset(Dataset):
+
+    """
+    Used for Health Index Regression (13-parameter output)
+    Loads data from a CSV containing:
+        image_path, param1, param2, ... param13
+    """
+
+
+
     def __init__(self, csv_path, transform=None):
         self.df = pd.read_csv(csv_path)
         self.transform = transform
@@ -47,3 +62,55 @@ class TransformerHealthDataset(Dataset):
 
     def __len__(self):
         return len(self.df)
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+# =======================
+#  PMT Classifier Dataset
+# =======================
+
+class PMTClassifierDataset(Dataset):
+    
+    
+    """
+    Used for PMT vs Non-PMT binary classification.
+    Folder structure:
+        root/
+            pmt/
+            non-pmt/
+    Labels:
+        non-pmt -> 0l
+        pmt     -> 1
+    """
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+        self.samples = []
+        self.classes = ["non-pmt", "pmt"]
+        self.class_to_idx = {cls: i for i, cls in enumerate(self.classes)}
+
+        # Load images
+        for cls in self.classes:
+            class_path = os.path.join(root_dir, cls)
+            if not os.path.isdir(class_path):
+                continue
+
+            for fname in os.listdir(class_path):
+                if fname.lower().endswith(("jpg", "jpeg", "png")):
+                    self.samples.append(
+                        (os.path.join(class_path, fname), self.class_to_idx[cls])
+                    )
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        img_path, label = self.samples[idx]
+        img = Image.open(img_path).convert("RGB")
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, label
